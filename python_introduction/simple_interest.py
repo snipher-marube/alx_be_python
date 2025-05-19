9c6cb748212743b10cdbfaa078195ea959385332
Snipher Marube <sniphermarube@gmail.com>
2025-05-19 10:11:51 +0300

    basic operations

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -2,6 +2,6 @@ principal = 1000  # Principal amount
 rate = 0.05  # Interest rate in decimal
 time = 3  # Time in years
 
-# Simple Interest formula
-interest = (principal * rate * time) 
-print("Simple Interest is:", interest)
+# calculate the interest
+interest = principal * rate * time
+print(f"The simple interest is: {interest}")
