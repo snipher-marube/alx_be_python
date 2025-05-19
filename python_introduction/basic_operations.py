39acd7780b13622e3102b2c6b1b2016f1ada32b2
Snipher Marube <sniphermarube@gmail.com>
2025-05-19 09:28:50 +0300

    basic operations

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,11 @@
+number1 = 10
+number2 = 5
+# Addition
+result = number1 + number2
+print("Addition:", result)
+# Subtraction
+result = number1 - number2
+print("Subtraction:", result)
+# Multiplication
+result = number1 * number2
+print("Multiplication:", result)
