d0743011194c7dec4245fbb0f9268e78cdb07862
Snipher Marube <sniphermarube@gmail.com>
2025-05-19 09:30:51 +0300

    basic operations

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,7 @@
+principal = 1000  # Principal amount
+rate = 5  # Interest rate in percentage
+time = 3  # Time in years
+
+# Simple Interest formula
+simple_interest = (principal * rate * time) / 100
+print("Simple Interest:", simple_interest)
