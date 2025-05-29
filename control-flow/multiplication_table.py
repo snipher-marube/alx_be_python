a5c7ada90eaf4fcf144a4e3b57ffa825f61b9ce2
Snipher Marube <sniphermarube@gmail.com>
2025-05-29 14:47:43 +0300

    multiplication table

---
+++ control-flow/multiplication_table.py
@@ -0,0 +1,6 @@
+# Get user input for the number
+number = int(input("Enter a number to see its multiplication table: "))
+
+# Generate and print the multiplication table using a for loop
+for i in range(1, 11):
+    print(f"{number} * {i} = {number * i}")
