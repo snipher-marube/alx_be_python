10545fddaced14825613047db2fe581f51df8c9c
Snipher Marube <sniphermarube@gmail.com>
2025-06-03 10:29:56 +0300

    arithemetic operations

---
+++ fns_and_dsa/arithmetic_operations.py
@@ -0,0 +1,15 @@
+def perform_operation(num1, num2, operation):
+    
+    match operation:
+        case "add":
+            return num1 + num2
+        case "subtract":
+            return num1 - num2
+        case "multiply":
+            return num1 * num2
+        case "divide":
+            if num2 == 0:
+                raise ValueError("Cannot divide by zero.")
+            return num1 / num2
+        case _:
+            raise ValueError("Invalid operation. Please use 'add', 'subtract', 'multiply', or 'divide'.")
